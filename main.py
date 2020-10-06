80204833ced0f8c38aa31754bc7e78ce8117c79c
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-06 12:41:56 -0300

    alterando modulo

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 
 import json, re, csv
 from app import app
+from config import mysql
 from flask import Flask, render_template, request, redirect, url_for, flash, stream_with_context, g, session
 from flask_restful import Resource, Api
 from flask_mysqldb import MySQL
