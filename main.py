9a0d12173ee0550e7ae205245bbd781028f16d9d
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-06 13:20:20 -0300

    alterando modulo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json, re, csv
 from app import app
-from config import mysql
+# from config import mysql
 from flask import Flask, render_template, request, redirect, url_for, flash, stream_with_context, g, session
 from flask_restful import Resource, Api
 from flask_mysqldb import MySQL
@@ -8,6 +8,16 @@ from flask_cors import CORS
 from flask_jsonpify import jsonify
 from json import dumps
 from io import StringIO
+
+mysql = MySQL()
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'my-secret-pw'
+app.config['MYSQL_DB'] = 'rest_emp'
+app.config['MYSQL_HOST'] = 'mysql'
+mysql.init_app(app)
+
+mysql = MySQL(app)
+
 
 @app.route('/add', methods=['POST'])
 def add_emp():
