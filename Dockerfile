f128b7db84dfc028bea2a4fffac208728c9f540f
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-06 22:08:38 -0300

    Adicionando arquivos Docker

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:5.5.5
+
+RUN apt-get update -y
+
+WORKDIR /opt
+
+ADD requirements.txt /opt
+RUN pip install -r requirements.txt
+
+ADD app.py /opt/
+ADD main.py /opt/
+ADD config.py /opt/
+
+COPY docker-entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+EXPOSE 5000
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+CMD ["python","main.py"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+help() {
+    echo "Usage: docker run -dti --link mysql:mysql --name dummy-api -e MYSQL_HOST=[value] -e MYSQL_USER=[value] -e MYSQL_PASS=[value] -e MYSQL_DB=[value]  image:tag" >&2
+    echo
+    echo "   MYSQL_HOST          hostname mysql database"
+    echo "   MYSQL_USER          user for connecting the mysql database"
+    echo "   MYSQL_PASS          passowrd of user for connecting the database"
+    echo "   MYSQL_DB            name database for connect "
+    echo
+    exit 1
+}
+
+if [ ! -z "$AD_SERVER" ] || [ ! -z "$USER_SVC" ] || [ ! -z "$PASS_SVC" ]|| [ ! -z "$API_DB" ]; then
+
+
+    sed -i "s/MYSQL_HOST/$MYSQL_HOST/g" /opt/config.py
+    sed -i "s/MYSQL_USER/$MYSQL_USER/g" /opt/config.py
+    sed -i "s/MYSQL_PASS/$MYSQL_PASS/g" /opt/config.py
+    sed -i "s/MYSQL_DB/$MYSQL_DB/g" /opt/config.py  
+
+else
+	echo "Please enter the required data!"
+	help
+fi
+
+exec "$@"
